58a249fbad129b4182d16fa1046dfa641ba4c55f
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2023-10-23 11:09:29 -0300

    Revert "testando commit"
    This reverts commit b6122d40822b8587b7bbf26073103d8a45cad875.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editando o <code>src/App.js</code> e salvando.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
