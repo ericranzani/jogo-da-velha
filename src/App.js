15c3e7e04b4b01f019edb1e25c84adbdabdb6869
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2023-10-23 13:14:34 -0300

    Solução do desafio, refatoração da pagina app.js, acrescentamos o estilo da font e seu estilo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className="Menu">
-          <span>Sobre</span>
-        </nav>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar o <code>src/App.js</code> e salvar para ter alterações.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendendo React
-        </a>
-      </header>
-    </div>
+    <a href="#" className="action">
+      Sobre
+    </a>
   );
 }
 
